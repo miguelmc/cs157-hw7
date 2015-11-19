6344e01bcad4a34773d3dd3eac3e8053c1b1e60e
Selby <selby@selbys-air.devices.brown.edu>
2015-11-18 23:36:42 -0500

    working in works wed night

--- arrangeCirclesRunner.m
+++ arrangeCirclesRunner.m
@@ -1,1 +1,14 @@
-% Empty file! Put whatever you want here
+x0=1000*randn(1,20); %initial guess, unfortunately far from the origin % NOTES FROM TA: write your own proposal functions; print out 41??
+%arrangeCirclesRunner
+%arrangeCircles(x0)
+%ans = 77 --> what this less than 41
+% ONLY CHANGE THE LINE BELOW: (replace the 0's with carefully chosen positive numbers)
+%epsilons=[10000 5000 1000 900 800 700 600 500 400 300 200 100 80 60 40 20 10 5 1 0.1];
+% pick a "schedule" for epsilon, that starts high, ends low, has perhaps
+% 15-20 epsilons, and, when run, optimizes the function to less than 1
+%for eps=epsilons,%loop through values in epsilons <-- must be a row vector
+    for i=1:1, %run localSearch for 3 seconds per epsilon, plotting every .3 seconds
+        x0=localSearch(@arrangeCircles,@wideScaleRandomNoiseMix3Proposal,(1:20),50,0,60,10,0.01);
+        figure(100); plot(x0); title(['eps=' num2str(eps)]);drawnow;
+    end
+%end
