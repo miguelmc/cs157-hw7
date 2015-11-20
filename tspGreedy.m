ecc406519b4430858cc1d486cb22cea94aba3b2e
Selby <selby@selbys-air.devices.brown.edu>
2015-11-19 21:09:02 -0500

    tsp greedy change

--- tspGreedy.m
+++ tspGreedy.m
@@ -1,23 +1,26 @@
 function out=tspGreedy(x)
 disp(x)
 
-currLocIndex = rand*100;
-disp(ceil(currLocIndex))
-nextLocIndex ;
-lowestLocIndex;
+currLocIndex = ceil(rand*100);
+disp(currLocIndex)
+nextLocIndex = 1;
+lowestLocIndex = 101;
 lowestDiff=inf;
 
+a=1:100;
+a(1)= x(currLocIndex)
 
-for i=1:100
-    for i=1:100
+for i=2:100 % indexing into new output a
+    for i=1:100 % travsering x to find lowest value
         tempDiff = abs(x(currLocIndex)-x(nextLocIndex));
         if tempDiff < lowestDiff 
             lowestDiff = tempDiff;
             lowestLocIndex = nextLocIndex;
         end
     end
+    a(i) = x(nextLocIndex);
     currLocIndex = nextLocIndex;
 end
 
-out = x;
+out = a;
 end
