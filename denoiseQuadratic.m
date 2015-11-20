e3324bfb9d4d5dba224aea30d3cd5091aba63d95
Selby <selby@selbys-air.devices.brown.edu>
2015-11-20 16:49:24 -0500

    5 working in hours

--- denoiseQuadratic.m
+++ denoiseQuadratic.m
@@ -17,14 +17,19 @@ A(1,2) = -1;
 B(1,1) = 1;
 B(2,1) = -1;
 
-fourierA = abs(fft(A)).^2;
-fourierB = abs(fft(B)).^2;
-fourierInp = fft(inp);
+fourierA = abs(fft2(A)).^2;
+fourierB = abs(fft2(B)).^2;
+fourierInp = fft2(inp);
 
-for i = 1:n(1)
-    for j = 1:n(2)
-        res(i,j) = ifft( fourierInp(i,j) ./ (k*( fourierA(i,j) + fourierB(i,j) ) + 1) );
-    end
-end
+% for i = 1:n(1)
+%     for j = 1:n(2)
+% %         res(i,j) = ifft( fourierInp(i,j) ./ (k*( fourierA(i,j) + fourierB(i,j) ) + 1) );
+%         res(i,j) = ifft( fourierInp(i,j) ./ (k*( fourierA(i,j) + fourierB(i,j) ) + 1) );
+% 
+%     end
+% end
 
-x = res;
+x = real(ifft2( fourierInp ./ (k*( fourierA + fourierB) + 1))) ;
+
+
+% x = real(res);
