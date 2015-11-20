58f65b23f92780b02ee6e7f6e6b89a45cedeee54
Miguel Martinez Chapa <mm193@cs.brown.edu>
2015-11-20 12:56:50 -0500

    changes a little p4

--- salesmanRunner.m
+++ salesmanRunner.m
@@ -23,7 +23,7 @@ best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,randperm(100),15,1,
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,15,1,100,5,85);
 
 disp('epsilon 10');
-best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,randperm(100),10,1,100,5,60);
+best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,10,1,100,5,60);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,10,1,100,5,60);
 
 % All these methods try to put one of the nodes of the longest edge on one end
@@ -59,33 +59,42 @@ end
 
 disp('epsilon 7');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,7,1,100,5,35);
-best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,7,1,100,10,35);
+best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,7,1,100,5,35);
+%best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,7,1,100,5,35);
 
 disp('epsilon 5');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,5,1,100,10,25);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,5,1,100,10,25);
+%best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,5,1,100,5,25);
 
 disp('epsilon 2');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,2,1,100,10,7);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,2,1,100,10,7);
+%best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,2,1,100,5,7);
+
 
 disp('epsilon 1');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,1,1,100,5,3);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,1,1,100,10,3);
+%best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,1,1,100,5,3);
 
 disp('epsilon .1');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,.1,1,100,5,.5);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,.1,1,100,10,.5);
+%best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,.1,1,100,5,.5);
 
 disp('epsilon .01');
 best = localSearch(@travelingSalesman,@swapTwoCitiesProposal,best,.01,1,100,5,.05);
 best = localSearch(@travelingSalesman,@swapTwoCitiesSmartProposal,best,.01,1,100,10,.05);
 
+
+best = localSearch(@travelingSalesman,@swapTwoNeighboringCitiesProposal,best,0,1,100,5,1);
+
 %%
 
-load cities
+%load cities
 
-x0=1:100; %initial guess, unfortunately far from the origin % NOTES FROM TA: write your own proposal functions; print out 41??
+%x0=1:100; %initial guess, unfortunately far from the origin % NOTES FROM TA: write your own proposal functions; print out 41??
 % try changing eplision 
 %arrangeCirclesRunner
 %arrangeCircles(x0)
@@ -95,9 +104,9 @@ x0=1:100; %initial guess, unfortunately far from the origin % NOTES FROM TA: wri
 % pick a "schedule" for epsilon, that starts high, ends low, has perhaps
 % 15-20 epsilons, and, when run, optimizes the function to less than 1
 %for eps=epsilons,%loop through values in epsilons <-- must be a row vector
-    for i=1:1, %run localSearch for 3 seconds per epsilon, plotting every .3 seconds
-        x0=localSearch(@travelingSalesman,@tspProposal,x0,10,-inf,inf,5,0.01);
-        figure(100); plot(x0); title(['eps=' num2str(eps)]);drawnow;
-    end
-    travelingSalesman(x0)
+%    for i=1:1, %run localSearch for 3 seconds per epsilon, plotting every .3 seconds
+%        x0=localSearch(@travelingSalesman,@tspProposal,x0,10,-inf,inf,5,0.01);
+%        figure(100); plot(x0); title(['eps=' num2str(eps)]);drawnow;
+%    end
+%    travelingSalesman(x0)
 %end
