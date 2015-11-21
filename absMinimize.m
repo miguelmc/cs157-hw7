ea3890f21958f01bc57be27db29e5c51828bef31
Selby <selby@selbys-air.devices.brown.edu>
2015-11-21 14:08:57 -0500

    1.3 good

--- absMinimize.m
+++ absMinimize.m
@@ -1,18 +1,12 @@
 x0=1000*randn(1,100); %initial guess, unfortunately far from the origin % 
-% how to run:
-% absMinimize
-% sum(abs(x0))
-% ans =
-% 
-%     0.2564
 % ONLY CHANGE THE LINE BELOW: (replace the 0's with carefully chosen positive numbers)
 epsilons=[5000 1000 900 800 700 600 500 400 300 200 100 80 60 40 20 10 5 1 0.1 0.01];
 % pick a "schedule" for epsilon, that starts high, ends low, has perhaps
 % 15-20 epsilons, and, when run, optimizes the function to less than 1
 for eps=epsilons,%loop through values in epsilons <-- must be a row vector
     for i=1:10, %run localSearch for 3 seconds per epsilon, plotting every .3 seconds
-        x0=localSearch(@(x)sum(abs(x)),@wideScaleRandomNoiseMix3Proposal,x0,eps,-inf,inf,.3,inf);
+        x0=localSearch(@(x)sum(abs(x)),@wideScaleRandomNoiseProposal,x0,eps,-inf,inf,.3,inf);
         figure(100); plot(x0); title(['eps=' num2str(eps)]);drawnow;
     end
-    disp(sum(abs(x0)))
+%     disp(sum(abs(x0)))
 end
