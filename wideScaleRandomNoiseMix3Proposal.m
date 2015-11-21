87bc62ff4dc6a4347088a15d88e1a9150ee3414d
Selby <selby@selbys-air.devices.brown.edu>
2015-11-21 14:25:07 -0500

    1.6 good

--- wideScaleRandomNoiseMix3Proposal.m
+++ wideScaleRandomNoiseMix3Proposal.m
@@ -2,8 +2,10 @@ function out=wideScaleRandomNoiseMix3Proposal(x)
 % Runs a random one of the three wideScaleRandomNoise proposal functions on
 % the input
 
-
+% Randomly picking a number in the integer range from 0 to 2 inclusive.
+% The if statement calls the cooresponding proposal.  
 num = floor(rand*3);
+
 if num == 0
     out = wideScaleRandomNoiseProposal(x);
 elseif num ==1
